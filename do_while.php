2448fdaf2f5c814dafcc315e89263b17fd638697
Emmett Peters <ejp8611@gmail.com>
2017-07-19 09:35:09 -0500

    added solutions for do_while.php

--- do_while.php
+++ do_while.php
@@ -1,8 +1,20 @@
 <?php
 
-$test = 5;
+$number = 0;
 
-while ($test <= 15){
-    echo "$test \n";
-    $test += 1;
-}
+do {
+    echo "$number \n";
+    $number += 2;
+} while ($number < 100);
+
+do {
+    echo "$number \n";
+    $number -= 5;
+} while ($number >= -10);
+
+$number2 = 2;
+
+do {
+    echo "$number2 \n";
+    $number2 = $number2 * $number2;
+} while ($number2 < 1000000);
