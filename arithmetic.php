4c13c32fd26ac0e3e31b8361b1d2578d9d171c92
Emmett Peters <ejp8611@gmail.com>
2017-07-21 16:12:11 -0500

    finished arithmetic exercise

---
+++ arithmetic.php
@@ -0,0 +1,44 @@
+<?php
+
+$a=15;
+4b=22;
+
+function add($a, $b)
+{
+    return $a + $b;
+}
+
+function subtract($a, $b)
+{
+    return $a - $b;
+}
+
+function multiply($a, $b)
+{
+    return $a * $b;
+}
+
+function divide($a, $b)
+{
+    return $a / $b;
+}
+
+function modulus($a, $b)
+{
+    return $a % $b;
+}
+
+$adding = add(3,6);
+echo "$adding\n";
+$subtracting = subtract(4,2);
+echo "$subtracting\n";
+$multiplying = multiply(4,2);
+echo "$multiplying\n";
+$divideing = divide(4,2);
+echo "$divideing\n";
+$modulusing = modulus(4,2);
+echo "$modulusing\n";
+
+
+
+// Add code to test your functions here
