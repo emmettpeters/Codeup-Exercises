17d497034f55ae837e9805ef57a542927d71e96b
Emmett Peters <ejp8611@gmail.com>
2017-07-21 11:45:10 -0500

    finished bonuses for foreach_books.php

--- foreach_books.php
+++ foreach_books.php
@@ -21,13 +21,17 @@ $books = array(
         'published' => 1859,
         'author' => 'Charles Dickens',
         'pages' => 544
+    ),
+     'Lord of the Rings' => array(
+        'published' => 1968,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 1178
     )
 );
 
 //completed and Bonuses
 
 foreach ($books as $key => $book){
-
     if ($book["published"] > 1950 && $book["pages"] < 300){
         echo "$key\n";
         foreach($book as $key2 => $value){
@@ -35,3 +39,14 @@ foreach ($books as $key => $book){
         }
     }
 }
+
+$pages=0;
+$yearTot=0;
+
+foreach ($books as $key => $book){
+    $pages = $pages + ((int)$book["pages"]);
+    $yearTot = $yearTot + ((int)$book["published"]);  
+}
+
+echo "Average Page count is => " . ($pages/count($books)) . PHP_EOL;
+echo "Average Year of Publishing is => " . ($yearTot/count($books)) . PHP_EOL;
