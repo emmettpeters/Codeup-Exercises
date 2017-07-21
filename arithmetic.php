e60c73b95cbc0615c8ae3003d0f44ff89c128c66
Emmett Peters <ejp8611@gmail.com>
2017-07-21 16:44:41 -0500

    finished both parts of arithmetic.php

--- arithmetic.php
+++ arithmetic.php
@@ -8,7 +8,7 @@ function add($a, $b)
 	if (is_numeric($a) && is_numeric($b)){
     	return $a + $b;
     } else{
-    	return "I need numbers dood!/n";
+    	return "I need numbers dood! Either $a or $b is NaN\n";
     }
 }
 
@@ -17,7 +17,7 @@ function subtract($a, $b)
     if (is_numeric($a) && is_numeric($b)){
     	return $a - $b;
     } else{
-    	return "I need numbers dood!/n";
+    	return "I need numbers dood! Either $a or $b is NaN\n";
     }
 }
 
@@ -26,20 +26,25 @@ function multiply($a, $b)
     if (is_numeric($a) && is_numeric($b)){
     	return $a * $b;
     } else{
-    	return "I need numbers dood!/n";
+    	return "I need numbers dood! Either $a or $b is NaN\n";
     }
 }
 
 function divide($a, $b){
-	if ($b != 0){
-	    if (is_numeric($a) && is_numeric($b)){
-	    	return $a / $b;
-	    } else {
-	    	return "I need numbers dood!/n";
-	    }
-	} else {
-		echo "cant divide by 0 FOOL!\n";
-	}
+	
+	// if ($b != 0){
+    if (is_numeric($a) && is_numeric($b)){
+    	return $a / $b;
+    } else {
+    	function error($a,$b){
+    		return "I need numbers dood! Either $a or $b is NaN\n";
+    	}
+    	return error($a,$b);
+    }
+	
+	// else {
+	// 	echo "cant divide by 0 FOOL!\n";
+	// }
 }
 
 function modulus($a, $b)
@@ -47,7 +52,7 @@ function modulus($a, $b)
     if (is_numeric($a) && is_numeric($b)){
     	return $a % $b;
     } else{
-    	return "I need numbers dood!/n";
+    	return "I need numbers dood! Either $a or $b is NaN\n";
     }
 }
 
@@ -57,7 +62,7 @@ $subtracting = subtract(4,2);
 echo "$subtracting\n";
 $multiplying = multiply(4,2);
 echo "$multiplying\n";
-$divideing = divide(4,0);
+$divideing = divide(4,"eggplant");
 echo "$divideing\n";
 $modulusing = modulus(4,2);
 echo "$modulusing\n";
