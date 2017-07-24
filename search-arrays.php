0552252ed28222e988de9ae79c8b950f779c06ed
Emmett Peters <ejp8611@gmail.com>
2017-07-24 16:58:01 -0500

    refactored correctly search-arrays.php

--- search-arrays.php
+++ search-arrays.php
@@ -6,16 +6,39 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 function nameInArray($name,$array){
 	
 	if (array_search("$name", $array) === false){
-		return "FALSE";
+		return false;
 	} else {
-		return "TRUE";
+		return true;
 	}
 }
-echo nameInArray('Tina',$names) . PHP_EOL;
+
+if(nameInArray('Tina',$names)){
+	echo "Tina is in the array\n";
+} else {
+	echo "Tina aint in the array\n";
+}
+
+if(nameInArray('Bob',$names)){
+	echo "Bob is in the array\n";
+} else {
+	echo "Bob aint in the array\n";
+}
+
+// function arrayCommons($array1,$array2){
+// 	$nameCount = count(array_intersect($array1,$array2));
+// 	return $nameCount;
+// }
+
+// echo arrayCommons($names,$compare) . PHP_EOL;
 
 function arrayCommons($array1,$array2){
-	$nameCount = count(array_intersect($array1,$array2));
+	$nameCount = 0;
+	foreach($array1 as $value){
+		if (nameInArray($value,$array2)){
+			$nameCount++;
+		};
+	}
 	return $nameCount;
 }
 
-echo arrayCommons($names,$compare) . PHP_EOL;
+echo "The number of value sharde between names and compare is " . arrayCommons($names,$compare) . PHP_EOL;
