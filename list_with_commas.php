769bbb84e5bf41975766f80bf47dc6bc0dd6b32b
Emmett Peters <ejp8611@gmail.com>
2017-07-25 14:13:55 -0500

    refactored the list exercise

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,40 +1,46 @@
 <?php
+
 $famousFakePhysicists = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner and Tony Stark';
 
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}\n";
 
+function humanizeList($string,$bool) {
+	if ($bool === true){
+
+	    $physArray = explode(", ",$string,5);
+	    asort($physArray);
+	    return $physArray;
+	}
+}
+
+$physArray = humanizeList($famousFakePhysicists,true);
+
+print_r($physArray) . PHP_EOL;
+
+$last = array_shift($physArray);
+
+echo $last . PHP_EOL;
+
+$lastArr = explode(" and ", $last);
+
+print_r($lastArr) . PHP_EOL;
+
+array_push($physArray,$lastArr[0],$lastArr[1]);
+
+print_r($physArray) . PHP_EOL;
+
+asort($physArray);
+
+print_r($physArray);
+
+function arrayToString($arr){
+	$newString = implode(",",$arr);
+	return $newString;
+}
+
+$newString = arrayToString($physArray);
+
+echo $newString;
 
 
-
-function humanizeList($string) {
-    $physArray = explode(", ",$string,5);
-    return $physArray;
- }
-
- $physArray = humanizeList($famousFakePhysicists);
-
- print_r($physArray) . PHP_EOL;
-
- $last = array_pop($physArray);
-
- echo $last . PHP_EOL;
-
-  $lastArr = explode(" and ", $last);
-
-  print_r($lastArr) . PHP_EOL;
-
-  array_push($physArray,$lastArr[0],$lastArr[1]);
-
-  print_r($physArray) . PHP_EOL;
-
-  asort($physArray);
-
-  print_r($physArray);
-
-// $ffff = (humanizedList($physicistsString));
-// $order = arsort($ffff);
-
-// echo "Some of the most famous fictional theoretical physicists are\n";
-
-// print_r($order) . PHP_EOL;
 
